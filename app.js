dec579589968ff7e75cfc06c34d1b1072b3a3b63
hidavehenry <hidavehenry@gmail.com>
2021-04-04 19:00:21 -0400

    added updated js and included assets folder

--- app.js
+++ app.js
@@ -1,76 +1,99 @@
-const dogApp = {};
 
-dogApp.key = `f0042504-b451-49fe-a1d7-d83d59145255`;
-userSelection = `blahBlah`
+const heightQuestion = document.querySelector("#question1");
+const weightQuestion = document.querySelector("#question2");
+const lifeQuestion = document.querySelector("#question3");
+const submitBtn = document.querySelector("#submit-btn");
+const resultsArea = document.querySelector("#results");
 
+const apiKey = `f0042504-b451-49fe-a1d7-d83d59145255`;
+const url = `https://api.thedogapi.com/v1/breeds?${apiKey}`;
 
-dogApp.init = () => {
-	dogApp.getPictures();
-};
+submitBtn.addEventListener("click", function (event) {
+	event.preventDefault();
+	getDogs();
+});
 
-// PHASE 1 - Fetching the Data
+function getDogs() {
+	while (resultsArea.firstChild) {
+		resultsArea.firstChild.remove();
+	}
 
-dogApp.getPictures = () => {
-	const url = new URL(`https://api.thedogapi.com/v1/breeds`);
+	const [
+		minHeightRequirement,
+		maxHeightRequirement,
+	] = heightQuestion.value.split("-").map((num) => parseInt(num));
 
-	url.search = new URLSearchParams({
-		key: dogApp.key,
-		image: true,
-        temperament: //tbd
-	});
+	const [
+		minWeightRequirement,
+		maxWeightRequirement,
+	] = weightQuestion.value.split("-").map((num) => parseInt(num));
+
+	// FINISH GETTING THIS PART TO WORK!
+	// const [
+	// 	minLifeRequirement,
+	// 	maxLifeRequirement,
+	// ] = lifeQuestion.value.split("-").map((num) => parseInt(num));
 
 	fetch(url)
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
-			dogApp.displayPictures(data);
+
+			const result = data.filter((dog) => {
+				const [minHeight, maxHeight] = dog.height.metric
+					.split("-")
+					.map((num) => parseInt(num));
+
+				const [minWeight, maxWeight] = dog.weight.metric
+					.split("-")
+					.map((num) => parseInt(num));
+
+			// FINISH THIS PART!!
+			// let lifeSpan = data.life_span.replace('years', ''); ?????
+
+				// const [minLife, maxLife] = dog.life_span.metric
+				// .split("-")
+				// .map((num) => parseInt(num));
+
+				if (minHeight >= minHeightRequirement) {
+					if (
+						maxHeight <= maxHeightRequirement ||
+						maxHeight === "undefined"
+					) 
+					if (minWeight >= minWeightRequirement) 
+						if (
+							maxWeight <= maxWeightRequirement ||
+							maxWeight === "undefined"
+						)
+					// // FINISH THIS PART TOOOOOOOOOOO!
+					// if (minLife >= minLifeRequirement) 
+					// 	if (
+					// 		maxLife <= maxLifeRequirement ||
+					// 		maxLife === "undefined"
+					// 	)
+						{
+						return dog;
+					}
+				}
+			});
+
+			// NEED TO ADD AN ERROR HANDLER FOR IF THERE ARE NO RESPONSES. RIGHT NOW ITS JUST BLANK.
+
+			result.forEach((dogObj) => {
+
+				// ADD IN ALT TEXT FOR IMAGES
+				// LOOK AT MAKING THE RESULTS SHOW UP IN THEIR OWN DIVS?
+				const element = document.createElement("img");
+				element.setAttribute("src", dogObj.image.url);
+				element.setAttribute("alt", dogObj.name);
+				const name = document.createElement("h2");
+				name.textContent = dogObj.name;
+				const temperament = document.createElement(`p`);
+				temperament.textContent = dogObj.temperament;
+				resultsArea.appendChild(element);
+				resultsArea.appendChild(name);
+				resultsArea.appendChild(temperament);
+			});
 		});
-};
-
-// PHASE 2 - Display the Data
-
-dogApp.displayPictures = (arrayOfDogs) => {
-	const resultElement = document.querySelector(`#result`);
-	resultElement.innerHTML = ``;
-
-	arrayOfDogs.forEach((dogObject) => {
-		const breeds = document.createElement(`div`);
-		breeds.classList.add("dogCard");
-		const name = document.createElement(`h2`);
-		name.textContent = dogObject.name;
-		const temperament = document.createElement(`p`);
-		temperament.textContent = dogObject.temperament;
-		const image = document.createElement(`img`);
-		image.src = dogObject.image.url;
-		image.alt = dogObject.name;
-		breeds.appendChild(name);
-		breeds.appendChild(temperament);
-		breeds.appendChild(image);
-		resultElement.appendChild(breeds);
-	});
-};
-
-// PHASE 3 - Return the API Result
-
-//Event Listener Function
-
-document.getElementbyId("button").addEventListener("click", function(){
-    dogApp.temperament = document.getElementById("smartOrStrong").value
-    dogApp.temperament = document.getElementById("stubbornOrDocile").value
-    dogApp.temperament = document.getElementById("activeOrCalm").value
-
-return dogObject();
-
-}) 
-
-
-
-
-dogApp.init();
-
-
-//notes:
-//re-write event listener
-//creating a function inside of event listener
-//
+}
